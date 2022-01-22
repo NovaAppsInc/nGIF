2e6947515391571f17d09b645c7a0904c058ecc3
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2022-01-22 13:17:53 -0500

    updated dependencies and dialog things

--- main.js
+++ main.js
@@ -189,7 +189,7 @@ app.on('ready', createWindow, () => {
         const dialogOpts = {
           type: 'info',
           buttons: ['Restart', 'Later'],
-          title: 'nGIFUpdate',
+          title: 'nGIF Update',
           message: process.platform === 'win32' ? releaseNotes : releaseName,
           detail: 'A new version has been downloaded. Restart the application to apply the updates.'
         }
