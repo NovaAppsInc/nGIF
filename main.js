298f1a2e7165bcba4ef2be32709d51902b1061fb
Nova <49733954+NovaAppsInc@users.noreply.github.com>
2022-02-07 05:22:28 -0500

    removed unused code

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // Modules to control application life and create native browser window
-require('update-electron-app')();
-const { create } = require('domain');
-const { app, BrowserWindow, autoUpdater ,globalShortcut, ipcMain, Menu, nativeTheme } = require('electron');
+const { app, BrowserWindow ,globalShortcut, ipcMain, Menu, nativeTheme } = require('electron');
 const path = require('path');
 const ipc = ipcMain;
 
@@ -50,6 +48,7 @@ function createWindow() {
     ipc.on("minApp", () => {
         mainWindow.minimize();
     });
+    
     ipc.on("maxApp", () => {
         if(mainWindow.isMaximized()) {
             mainWindow.webContents.send("changeIr");
@@ -58,106 +57,6 @@ function createWindow() {
             mainWindow.webContents.send("changeImx");
             mainWindow.maximize();
         }
-    });
-
-    function createWindowVer() {
-        verWin = new BrowserWindow({
-            width: 400,
-            height: 300,
-            minWidth: 400,
-            minHeight: 300,
-            darkTheme: true,
-            backgroundColor: '#222222',
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-                devTools: true,
-                preload: path.join(__dirname, 'preload.js'),
-            },
-            scrollBounce: true,
-            icon: "./icon.ico"
-        });
-
-        var menu = Menu.buildFromTemplate([
-            {
-                label: 'Quit',
-                click() {
-                    verWin.close();
-                }
-            }
-        ]);
-
-        Menu.setApplicationMenu(menu); 
-
-        ipc.on("closeAppVer", () => {
-            verWin.close();
-        });
-
-        verWin.loadFile('ver.html');
-
-        // Open the DevTools.
-        // mainWindow.webContents.openDevTools();
-    
-        // Emitted when the window is closed.
-        verWin.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        verWin = null;
-        });
-    }
-
-    function createGallery() {
-    // Create the browser window.
-        galleryWin = new BrowserWindow({
-            width: 855,
-            height: 655,
-            minWidth: 855,
-            minHeight: 655,
-            frame: true,
-            darkTheme: true,
-            thickFrame: true,
-            autoHideMenuBar: true,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-                devTools: true,
-                preload: path.join(__dirname, 'preload.js'),
-            },
-            scrollBounce: true,
-            icon: "./icon.ico"
-        });
-
-        var menu = Menu.buildFromTemplate([
-            {
-                label: 'Quit',
-                role: 'close',
-                role: 'quit'
-            }
-        ]);
-
-        Menu.setApplicationMenu(menu); 
-
-        galleryWin.loadFile('gallery.html');
-
-        // Open the DevTools.
-        galleryWin.webContents.openDevTools();
-    
-        // Emitted when the window is closed.
-        galleryWin.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        galleryWin = null;
-        });
-    }
-
-    ipc.on("gall", () => {
-        createGallery();
-    });
-
-    ipc.on("ver", ()=> {
-        createWindowVer();
     });
 
     // and load the index.html of the app.
@@ -184,25 +83,6 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow, () => {
     // Menu.setApplicationMenu(null)
-    autoUpdater.checkForUpdates();
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-        const dialogOpts = {
-          type: 'info',
-          buttons: ['Restart', 'Later'],
-          title: 'nGIF Update',
-          message: process.platform === 'win32' ? releaseNotes : releaseName,
-          detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-        }
-      
-        dialog.showMessageBox(dialogOpts).then((returnValue) => {
-          if (returnValue.response === 0) autoUpdater.quitAndInstall()
-        })
-    })
-    
-    autoUpdater.on('error', message => {
-      console.error('There was a problem updating the application')
-      console.error(message)
-    });
 });
 
 // Quit when all windows are closed.
@@ -227,6 +107,6 @@ app.on('activate', function() {
 
 // app.whenReady().then(() => {
 //     globalShortcut.register('Shift+CommandOrControl+I', () => {
-//         return;
-//     })
+//         return null;
+//     });
 // })
